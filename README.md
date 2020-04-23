d73185638e7f5373bbaf0855bae3bdeb66258366
MinoMunMun <wunonglin@gmail.com>
2020-04-23 14:31:13 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,30 @@
+# WeiboPictureDownload
+
+## How to use
+
+#### Download
+```text
+git clone https://github.com/MunMunMiao/WeiboPictureDownload.git
+```
+
+#### Install dependencies
+```text
+npm install
+```
+
+#### Quick start
+```text
+node weibo.js -c XXXXX -u 518178935
+```
+
+## Options
+```text
+Usage: weibo [options]
+
+Options:
+  -u, --uid <number>        Weibo user object id
+  -d, --directory <string>  Output directory (default: "./")
+  -t, --threads <number>    Download threads (default: 10)
+  -o, --output-file         Output files
+  -h, --help                output usage information
+```
---
+++ weibo.js
@@ -0,0 +1,191 @@
+const fs = require('fs')
+const path = require('path')
+const axios = require('axios')
+const commander = require('commander')
+const { from, Observable } = require('rxjs')
+const { mergeMap, switchMap } = require('rxjs/operators')
+const progressBar = require('progress')
+
+commander
+    .requiredOption('-u, --uid <number>', 'Weibo user object id')
+    .requiredOption('-c, --cookie <number>', 'Weibo SUB')
+    .requiredOption('-d, --directory <string>', 'Output directory', './')
+    .option('-t, --threads <number>', 'Download threads',10)
+    .option('-o, --output-file', 'Output files')
+
+commander.parse(process.argv)
+
+let weiboUid = null
+let weiboToken = null
+const userAgent = `Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.122 Safari/537.36`
+let thread = 5
+let outputFile = false
+let directory = null
+
+if (commander.uid){
+    weiboUid = commander.uid
+}
+if (commander.cookie){
+    weiboToken = commander.cookie
+}
+if (commander.directory){
+    directory = path.join(commander.directory, weiboUid)
+}
+if (commander.threads){
+    thread = Number(commander.threads)
+}
+if (commander.outputFile){
+    outputFile = true
+}
+
+console.log('Input config')
+console.table({
+    uid: weiboUid,
+    token: weiboToken,
+    'output directory': directory,
+    thread,
+    'output files': outputFile
+})
+
+function writeUrlFile(items){
+    let str = ''
+
+    for (const item of items){
+        str += `${ item }\n`
+    }
+
+    fs.writeFileSync(path.join(directory, `weibo-${ weiboUid }.txt`), str)
+}
+
+function asyncWrite(path, data) {
+    return new Observable(subscriber => {
+        fs.writeFile(path, data, err => {
+            if (err){
+                subscriber.error(err)
+            }else {
+                subscriber.next()
+                subscriber.complete()
+            }
+        })
+    })
+}
+
+function download(items) {
+    let currentIndex = 0
+    const bar = new progressBar(`Downloading [:bar] :current/:total :percent`, {
+        curr: currentIndex,
+        total: items.length,
+        clear: true
+    })
+
+    from(items)
+        .pipe(
+            mergeMap(item => {
+                return from(axios.get(item, {
+                    responseType: 'arraybuffer',
+                    headers: {
+                        'User-Agent': userAgent
+                    }
+                })).pipe(switchMap(result => {
+                    const filename = result.request.path.match(/[a-zA-Z0-9.]*$/gi)[0]
+                    return asyncWrite(path.join(directory, filename), result.data)
+                }))
+            }, thread))
+        .subscribe({
+            next: result => {
+                bar.tick()
+            },
+            complete: () => {
+                process.exit()
+            }
+        })
+}
+
+async function getUserData(){
+    try {
+        const result = await axios.get('https://m.weibo.cn/api/container/getIndex', {
+            params: {
+                type: 'uid',
+                value: weiboUid
+            }
+        })
+
+        if (result.data.ok === 1){
+            console.table({
+                id: result.data.data.userInfo.id,
+                name: result.data.data.userInfo.screen_name,
+                verifiedReason: result.data.data.userInfo.verified_reason || 'Null',
+                description: result.data.data.userInfo.description,
+                weiboCount: result.data.data.userInfo.statuses_count,
+                followCount: result.data.data.userInfo.follow_count,
+                followersCount: result.data.data.userInfo.followers_count,
+            })
+        }else {
+            throw `Error getting user information`
+        }
+    }catch (err) {
+        console.error(err)
+    }
+}
+
+async function getUserWeiboPictures(){
+    let page = 1
+    let url = []
+    let stop = false
+
+    while (!stop){
+        try {
+            const http = await axios.get('https://m.weibo.cn/api/container/getIndex', {
+                params: {
+                    count: 20,
+                    page,
+                    containerid: `107603${ weiboUid }`
+                },
+                headers: {
+                    cookie: `SUB=${ weiboToken }`
+                }
+            })
+
+            const result = http.data
+            if (result.ok === 1){
+                const miao = result.data.cards.filter(item => item.card_type === 9)
+                let pics = []
+
+                for (const item of miao){
+                    if (item.mblog.pics){
+                        pics = [...pics, ...item.mblog.pics]
+                    }
+                    if (item.mblog.retweeted_status && item.mblog.retweeted_status.pics){
+                        pics = [...pics, ...item.mblog.retweeted_status.pics]
+                    }
+                }
+
+                const pictures = pics.map(item => item.large.url)
+
+                url = [...url, ...pictures]
+                page += 1
+                console.log(`Analyze picture: ${ url.length }`)
+            }else {
+                stop = true
+            }
+        }catch (err) {
+            throw err
+        }
+    }
+
+    fs.mkdirSync(directory, { recursive: true })
+    writeUrlFile(url)
+
+    if (outputFile){
+        download(url)
+    }else {
+        process.exit()
+    }
+}
+
+async function main(){
+    await getUserData()
+    await getUserWeiboPictures()
+}
+
+main()
