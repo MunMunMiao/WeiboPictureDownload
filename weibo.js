8c3d7f28086621628c44931759946a8830e97a3a
MunMun Miao <18216142+MunMunMiao@users.noreply.github.com>
2023-10-19 10:32:12 +0800

    remove download

--- weibo.js
+++ weibo.js
@@ -1,9 +1,9 @@
 const fs = require('fs')
 const path = require('path')
-const axios = require('axios')
 const commander = require('commander')
 const {from, Observable} = require('rxjs')
 const {mergeMap, switchMap} = require('rxjs/operators')
+const {fromFetch} = require('rxjs/fetch')
 const progressBar = require('progress')
 
 commander
@@ -11,7 +11,6 @@ commander
 	.requiredOption('-c, --cookie <number>', 'Weibo SUB')
 	.requiredOption('-d, --directory <string>', 'Output directory', './')
 	.option('-t, --threads <number>', 'Download threads', 10)
-	.option('-o, --output-file', 'Output files')
 	.option('-i, --interval <number>', 'Analyze interval', 1200)
 	.option('-l, --limit <number>', 'Limit the number of query pages')
 
@@ -21,7 +20,6 @@ let weiboUid = null
 let weiboToken = null
 const userAgent = `Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.122 Safari/537.36`
 let thread = 5
-let outputFile = false
 let directory = null
 let interval = null
 let limit = null
@@ -44,9 +42,6 @@ if (commander.interval) {
 if (commander.limit) {
 	limit = Number(commander.limit)
 }
-if (commander.outputFile) {
-	outputFile = true
-}
 
 console.log('Input config')
 console.table({
@@ -56,7 +51,6 @@ console.table({
 	thread,
 	interval,
 	limit,
-	'output files': outputFile
 })
 
 function writeUrlFile(items) {
@@ -71,50 +65,6 @@ function writeUrlFile(items) {
 
 function delay(time) {
 	return new Promise(resolve => setTimeout(() => resolve(), time))
-}
-
-function asyncWrite(path, data) {
-	return new Observable(subscriber => {
-		fs.writeFile(path, data, err => {
-			if (err) {
-				subscriber.error(err)
-			} else {
-				subscriber.next()
-				subscriber.complete()
-			}
-		})
-	})
-}
-
-function download(items) {
-	let currentIndex = 0
-	const bar = new progressBar(`Downloading [:bar] :current/:total :percent`, {
-		curr: currentIndex,
-		total: items.length,
-		clear: true
-	})
-
-	from(items)
-		.pipe(
-			mergeMap(item => {
-				return from(axios.get(item, {
-					responseType: 'arraybuffer',
-					headers: {
-						'User-Agent': userAgent
-					}
-				})).pipe(switchMap(result => {
-					const filename = result.request.path.match(/[a-zA-Z0-9.]*$/gi)[0]
-					return asyncWrite(path.join(directory, filename), result.data)
-				}))
-			}, thread))
-		.subscribe({
-			next: result => {
-				bar.tick()
-			},
-			complete: () => {
-				process.exit()
-			}
-		})
 }
 
 async function getUserData() {
@@ -184,7 +134,6 @@ async function getUserWeiboPictures() {
 						pics = [...pics, ...item.mblog.retweeted_status.pics]
 					}
 
-
 					if (!!item?.mblog?.pics && !Array.isArray(item?.mblog?.pics) && typeof item?.mblog?.pics === "object") {
 						pics = [...pics, ...Object.values(item.mblog.pics)]
 					}
@@ -195,9 +144,9 @@ async function getUserWeiboPictures() {
 				}
 
 				const pictures = []
-				for (const item of pics){
+				for (const item of pics) {
 					const u = item?.large?.url
-					if (/\/\/wx(1|2|3|4).sinaimg/ig.test(u)){
+					if (/\/\/wx(1|2|3|4).sinaimg/ig.test(u)) {
 						pictures.push(u)
 					}
 				}
@@ -219,12 +168,6 @@ async function getUserWeiboPictures() {
 
 	fs.mkdirSync(directory, {recursive: true})
 	writeUrlFile(url)
-
-	if (outputFile) {
-		download(url)
-	} else {
-		process.exit()
-	}
 }
 
 async function main() {
